46a0b0e4b8dc75d16d3eba675fe8f5d5414da056
Nadif Dzaikra <nadifdzaikra@gmail.com>
2024-07-18 11:38:46 +0700

    change url merge portofolio

--- src/midleware/api.ts
+++ src/midleware/api.ts
@@ -793,7 +793,7 @@ const Raport = {
   ): AxiosPromise<any> =>
     instance({
       method: "PUT",
-      url: `/api/portofolio-report/${id}`,
+      url: `/api/portofolio-report/merge/${id}`,
       headers: {
         Authorization: `Bearer ${token}`,
       },
