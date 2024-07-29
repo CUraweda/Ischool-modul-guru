e537d740ec9334e2336f31ac2d89c7ab0c640ace
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-07-29 20:52:17 +0700

    feat: fetch me

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -1,16 +1,43 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import bg from "../assets/bg2.png";
-import { Store } from "../store/Store";
+import { employeeStore, Store } from "../store/Store";
+import { Auth } from "../midleware/api";
 
 interface Props {
   children?: React.ReactNode;
   name?: string;
 }
 const Layout: FC<Props> = ({ children }) => {
-  const { role } = Store();
+  const { role, token } = Store();
+  const { setEmployee, setHeadmaster, setFormTeachers, setFormSubjects, setFormXtras } = employeeStore()
   const Role = role ? parseInt(role, 10) : 0;
+
+  const getMe = async () => {
+    try {
+      const res = await Auth.MeData(token)
+
+      const {
+        id,
+        full_name,
+        headmaster,
+        formextras,
+        formsubjects,
+        formteachers,
+      } = res.data.data?.employee ?? {};
+
+      if (id && full_name) setEmployee({ id, full_name });
+      if (headmaster) setHeadmaster(headmaster);
+      if (formteachers) setFormTeachers(formteachers);
+      if (formsubjects) setFormSubjects(formsubjects);
+      if (formextras) setFormXtras(formextras);
+    } catch {}
+  }
+
+  useEffect(() => {
+    getMe()
+  }, [])
 
   return (
     <>
