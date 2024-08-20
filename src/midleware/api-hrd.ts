ec586323a40068c3ba8e5d05e9c9b384560379ee
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-08-20 14:29:10 +0700

    feat: pengajuan cuti izin

--- src/midleware/api-hrd.ts
+++ src/midleware/api-hrd.ts
@@ -5,14 +5,55 @@ const instance = axios.create({
 });
 
 const CutiIzin = {
-  create: (token: string | null, data: any) =>
+  showAll: (
+    token: string | null,
+    search: string,
+    employeeId: string,
+    type: string,
+    date: string,
+    status: string,
+    page: number = 0,
+    limit: number = 10
+  ) =>
     instance({
-      method: "POST",
-      url: "/api/for-country/create",
+      method: "GET",
+      url: "/api/employee-vacation",
+      params: {
+        search,
+        employee_id: employeeId,
+        type,
+        date,
+        status,
+        page,
+        limit,
+      },
       headers: {
         Authorization: `Bearer ${token}`,
       },
-      data,
+    }),
+  showOne: (token: string | null, id: string) =>
+    instance.get("/api/employee-vacation/" + id, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
+  request: (token: string | null, data: any) =>
+    instance.post("/api/employee-vacation/request", data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
+  change: (token: string | null, id: string, data: any) =>
+    instance.put("/api/employee-vacation/change/" + id, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
+  remove: (token: string | null, id: string) =>
+    instance.delete("/api/employee-vacation/remove/" + id, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }),
 };
 
