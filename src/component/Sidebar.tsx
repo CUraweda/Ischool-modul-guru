3a271a05305257947a8f9c0ed548c9c7b4f01b2a
Muhammad Hasbi <hasbi@akasia.id>
2024-11-13 06:39:31 +0800

    chore: add change into dev branch

--- src/component/Sidebar.tsx
+++ src/component/Sidebar.tsx
@@ -83,131 +83,125 @@ const Sidebar = () => {
   }, [Role]);
 
   return (
-    <div>
-      <div className="drawer lg:drawer-open">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-side">
-          <label
-            htmlFor="my-drawer-2"
-            aria-label="close sidebar"
-            className="drawer-overlay"
-          />
-          <ul className="menu p-4 w-80 bg-base-100 min-h-screen">
-            <div className="w-full flex justify-between mb-3 items-center  pb-6">
-              <div className="flex justify-center items-center gap-1">
-                <img src={logo} alt="logo" className="w-20" />
-                <p className="sm:text-xl text-xl font-bold">
-                  Sekolah Alam Depok
-                </p>
-              </div>
-              <label
-                htmlFor="my-drawer-2"
-                className="btn btn-ghost text-3xl font-bold lg:hidden"
-              >
-                <BsListNested />
-              </label>
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        />
+        <ul className="menu p-4 w-80 bg-base-100 min-h-screen">
+          <div className="w-full flex justify-between mb-3 items-center  pb-6">
+            <div className="flex justify-center items-center gap-1">
+              <img src={logo} alt="logo" className="w-20" />
+              <p className="sm:text-xl text-xl font-bold">Sekolah Alam Depok</p>
             </div>
-            <select
-              value={academicYear}
-              onChange={(e) => setAcademicYear(e.target.value)}
-              className="text-small select select-bordered w-full mb-6"
+            <label
+              htmlFor="my-drawer-2"
+              className="btn btn-ghost text-3xl font-bold lg:hidden"
             >
-              {years.map((year, i) => (
-                <option
-                  key={i}
-                  className="text-small"
-                  value={year.name}
-                  selected={year.status == "Aktif"}
-                >
-                  {year.name}
-                </option>
-              ))}
-            </select>
-            <ul className="menu font-bold rounded-lg max-w-xs w-full text-gray-500">
-              {data
-                .filter((item) => !item.hide)
-                .filter((item) => !(item.title === "Penilaian" && !isAsessor))
-                .filter((item) =>
-                  item.roles && role
-                    ? item.roles.map((r) => r.toString()).includes(role)
-                    : true
-                )
-                .map((item: Menu, index: number) => (
-                  <React.Fragment key={`menu-` + index}>
-                    {item.submenu ? (
-                      <li className="my-2">
-                        <details>
-                          <summary>
-                            <span className="text-2xl">
-                              {iconMapping[item.icon]}
+              <BsListNested />
+            </label>
+          </div>
+          <select
+            value={academicYear}
+            onChange={(e) => setAcademicYear(e.target.value)}
+            className="text-small select select-bordered w-full mb-6"
+          >
+            {years.map((year, i) => (
+              <option
+                key={i}
+                className="text-small"
+                value={year.name}
+                selected={year.status == "Aktif"}
+              >
+                {year.name}
+              </option>
+            ))}
+          </select>
+          <ul className="menu font-bold rounded-lg max-w-xs w-full text-gray-500">
+            {data
+              .filter((item) => !item.hide)
+              .filter((item) => !(item.title === "Penilaian" && !isAsessor))
+              .filter((item) =>
+                item.roles && role
+                  ? item.roles.map((r) => r.toString()).includes(role)
+                  : true
+              )
+              .map((item: Menu, index: number) => (
+                <React.Fragment key={`menu-` + index}>
+                  {item.submenu ? (
+                    <li className="my-2">
+                      <details>
+                        <summary>
+                          <span className="text-2xl">
+                            {iconMapping[item.icon]}
+                          </span>
+                          <a>{item.title}</a>
+                          {item.badge && (
+                            <span className="badge badge-warning badge-sm">
+                              {item.badge}
                             </span>
-                            <a>{item.title}</a>
-                            {item.badge && (
-                              <span className="badge badge-warning badge-sm">
-                                {item.badge}
-                              </span>
-                            )}
-                          </summary>
-                          <ul>
-                            {item.subtitle
-                              ?.filter((s) => !s.hide)
-                              .map((Item: subtitle, Index: number) => (
-                                <Link to={Item.url} key={`link-` + Index}>
-                                  <li
-                                    key={`subtitle-` + Index}
-                                    className={`my-2 transition duration-200 rounded-md ${
-                                      activeMenuItem === Item.url
-                                        ? "bg-blue-100 text-blue-600"
-                                        : ""
-                                    }`}
-                                    onClick={() =>
-                                      handleMenuItemClick(Item.url)
-                                    }
-                                  >
-                                    <p>
-                                      {Item.name}
-                                      {Item.badge && (
-                                        <span className="badge badge-warning badge-sm">
-                                          {Item.badge}
-                                        </span>
-                                      )}
-                                    </p>
-                                  </li>
-                                </Link>
-                              ))}
-                          </ul>
-                        </details>
+                          )}
+                        </summary>
+                        <ul>
+                          {item.subtitle
+                            ?.filter((s) => !s.hide)
+                            .map((Item: subtitle, Index: number) => (
+                              <Link to={Item.url} key={`link-` + Index}>
+                                <li
+                                  key={`subtitle-` + Index}
+                                  className={`my-2 transition duration-200 rounded-md ${
+                                    activeMenuItem === Item.url
+                                      ? "bg-blue-100 text-blue-600"
+                                      : ""
+                                  }`}
+                                  onClick={() => handleMenuItemClick(Item.url)}
+                                >
+                                  <p>
+                                    {Item.name}
+                                    {Item.badge && (
+                                      <span className="badge badge-warning badge-sm">
+                                        {Item.badge}
+                                      </span>
+                                    )}
+                                  </p>
+                                </li>
+                              </Link>
+                            ))}
+                        </ul>
+                      </details>
+                    </li>
+                  ) : (
+                    <Link to={item.url} key={`link-` + index}>
+                      <li
+                        className={`my-2 transition duration-200 rounded-md ${
+                          activeMenuItem === item.url
+                            ? "bg-blue-100 text-blue-600"
+                            : ""
+                        }`}
+                        onClick={() => handleMenuItemClick(item.url)}
+                      >
+                        <div>
+                          <span className="text-2xl">
+                            {iconMapping[item.icon]}
+                          </span>
+                          <p>{item.title}</p>
+                          {item.badge && (
+                            <span className="badge badge-warning badge-sm">
+                              {item.badge}
+                            </span>
+                          )}
+                        </div>
                       </li>
-                    ) : (
-                      <Link to={item.url} key={`link-` + index}>
-                        <li
-                          className={`my-2 transition duration-200 rounded-md ${
-                            activeMenuItem === item.url
-                              ? "bg-blue-100 text-blue-600"
-                              : ""
-                          }`}
-                          onClick={() => handleMenuItemClick(item.url)}
-                        >
-                          <div>
-                            <span className="text-2xl">
-                              {iconMapping[item.icon]}
-                            </span>
-                            <p>{item.title}</p>
-                            {item.badge && (
-                              <span className="badge badge-warning badge-sm">
-                                {item.badge}
-                              </span>
-                            )}
-                          </div>
-                        </li>
-                      </Link>
-                    )}
-                  </React.Fragment>
-                ))}
-            </ul>
-            <div className="mt-auto"></div>
+                    </Link>
+                  )}
+                </React.Fragment>
+              ))}
           </ul>
-        </div>
+          <div className="mt-auto"></div>
+        </ul>
       </div>
     </div>
   );
